a21d50f4d5e9e53b8852445c8d3baf46035571d1
Gyanthakur <gps.96169@gmail.com>
2024-06-13 23:48:56 +0530

    feat:added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Navbar from "@/components/ui/common-layout/navbar";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<section>
-			main content
+			<Navbar/>
 		</section>
 	);
 }
