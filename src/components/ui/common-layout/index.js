b149d173201ef3b7dee1ef7c88254cf02b434269
Gyanthakur <gps.96169@gmail.com>
2024-06-14 22:40:16 +0530

    feat:update header

--- src/components/ui/common-layout/index.js
+++ src/components/ui/common-layout/index.js
@@ -4,7 +4,7 @@ function CommonLayout ({childern}){
     return <div className="mx-auto max-w-7xl p-6 lg:px-8 ">
 
         {/* header component */}
-
+        Header
         {/* main component */}
         <main>{childern}</main>
 
