c015cd315435966a45735e413d29004938734086
Gyanthakur <gps.96169@gmail.com>
2024-06-30 20:12:50 +0530

    feat: added activity,companies,jobs,membership...

---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const connectToDB = async () => {
+  const connectionURL = process.env.MONGODB_URL;
+
+  mongoose
+    .connect(connectionURL)
+    .then(() => console.log("jon board database connection is successfull"))
+    .catch((error) => console.log(error, "not connected to database"));
+};
+
+export default connectToDB;
